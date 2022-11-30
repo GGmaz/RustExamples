f167a20f88f4b4aa32e5ade53af09b84b85c75b0
GGmaz <kajtes.nikola@gmail.com>
2022-11-30 01:49:05 +0100

    enable deleting, poping, size

--- practise/src/main.rs
+++ practise/src/main.rs
@@ -62,7 +62,7 @@ impl SortedList {
     fn print(&mut self) {
         let mut newList = SortedList {
             value: self.value.clone(),
-            next: self.next.take(),
+            next: self.next.clone(),
         } ;
 
         let mut temp = Some(Box::new(newList)); 
@@ -77,6 +77,49 @@ impl SortedList {
             }
         }
     }
+
+    fn delete(&mut self, id:i32) {  
+        match self.next {
+            Some(ref mut nextNode) => {
+                if self.value.id == id {
+                    self.value = nextNode.value.clone();
+                    self.next = nextNode.next.take();
+                }
+                else {
+                    match nextNode.next {
+                        Some(ref s) => {
+                            nextNode.delete(id);
+                        },
+                        None => {
+                            if nextNode.value.id == id {
+                                self.next = None;
+                            }
+                        }
+                    }
+                } 
+            },
+            None => { println!("There is no such transaction with id: {id}"); }
+        }
+    } 
+
+    fn pop(&mut self) {  
+        match self.next {
+            Some(ref mut nextNode) => {
+                self.value = nextNode.value.clone();
+                self.next = nextNode.next.clone();
+            },
+            None => { println!("Prazna lista"); }
+        }
+    } 
+
+    fn size(&mut self, i:i32) -> i32 {  
+        match self.next {
+            Some(ref mut nextNode) => {
+                nextNode.size(i+1)
+            },
+            None => { i }
+        }
+    } 
 }
 
 
@@ -86,9 +129,15 @@ fn main() {
         next: None,
     };
 
-    //list.print();
+    
     list.insert(Transaction { id: 435, sender: "Pero".to_string(), receiver: "Mika".to_string(), sum: 43 });
     list.insert(Transaction { id: 5, sender: "Mika".to_string(), receiver: "Momo".to_string(), sum: 3 });
     list.insert(Transaction { id: 54, sender: "Mika".to_string(), receiver: "Pero".to_string(), sum: 49 });
+    //list.print();
+    println!(" ");
+    list.delete(23);
     list.print();
+    list.pop();
+    list.print();
+    println!("size {}", list.size(1));
 }
